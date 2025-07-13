f95d8fc4722d3058fdb411c96d381f00a0218e62
Loris Jared Ndonga <130836683+ndongaloris@users.noreply.github.com>
2025-07-13 03:02:32 +0300

    array file fixed (length)

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -14,7 +14,7 @@ public static class Arrays
         // be implemented by another person.
 
         // Creating an array where I will storing my new data
-        double arraysMultiples = new[] {1,2};
+        var arraysMultiples = new double [length];
         // storing the source number first before adding the extras
         arraysMultiples[0]= number;
         // looping  to be able to add the extra number into the array
